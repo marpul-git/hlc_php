3b558ef7fee9db987a34594504479ab80f5c68d5
Mario <marpul3@hotmail.com>
2021-03-05 19:12:23 +0100

    Index modificado

--- Proyecto_Galeria_PHP/galeria/index.php
+++ Proyecto_Galeria_PHP/galeria/index.php
@@ -4,15 +4,13 @@
   include "common/config.php";
   include "common/mysql.php";
 
-  /*include "includes/config.php";
-
-  debug ( $config);*/
+ 
 
 
   # conectamos con la base de datos
   $connection = Connect( $config['database']);
 
-  $sql  = "select * from images where enabled = 1 order by id desc";
+  $sql  = "select * from images where enabled = 1 order by id asc";
 
   $rows = ExecuteQuery( $sql, $connection);
 
@@ -31,7 +29,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Thumbnail Gallery - Start Bootstrap Template</title>
+    <title>Galeria de imagenes</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -39,12 +37,7 @@
     <!-- Custom CSS -->
     <link href="assets/bootstrap/css/thumbnail-gallery.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+   
 
 </head>
 
@@ -110,7 +103,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p></p>
+                    <p> @migaleriadefotos</p>
                 </div>
             </div>
         </footer>
